5c1958194b10feb9a79409ae2daad9a39e563ae2
CrypDaddy <165932837+CrypDaddy@users.noreply.github.com>
2024-04-14 09:11:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const solanaWeb3 = require("@solana/web3.js");
 console.log(solanaWeb3);
 import * as solanaWeb3 from "@solana/web3.js";
 console.log(solanaWeb3);
+// Import the Keypair class from the @solana/web3.js library
+const { Keypair } = require("@solana/web3.js");
+
+// Generate a new Keypair
+let keypair = Keypair.generate();
+
+// Log the generated keypair to the console
+console.log(keypair);
